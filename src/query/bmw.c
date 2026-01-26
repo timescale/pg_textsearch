5876383650ef026914ec0539ce2b5298cce74029
Kern3l_Krunch <kern3l_krunch@midgardcyber.io>
2026-01-26 13:49:16 -0500

    Update copyright years to 2025-2026
    Updates copyright statements from 2025 to 2025-2026 in all source files
    as requested in issue #178.
    
    Related: #178

--- src/query/bmw.c
+++ src/query/bmw.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Tiger Data, Inc.
+ * Copyright (c) 2025-2026 Tiger Data, Inc.
  * Licensed under the PostgreSQL License. See LICENSE for details.
  *
  * bmw.c - Block-Max WAND query optimization implementation
