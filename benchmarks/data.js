170860a80ddf7a0c16f8018f9406db49269b3524
github-action-benchmark <github@users.noreply.github.com>
2026-02-28 02:30:13 +0000

    add wikipedia_gin Benchmarks (customSmallerIsBetter) benchmark result for 7d0b7056e70944c45d2e374121b02421cf5afe3d

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772245812150,
+  "lastUpdate": 1772245813517,
   "repoUrl": "https://github.com/timescale/pg_textsearch",
   "entries": {
     "cranfield Benchmarks": [
@@ -18340,6 +18340,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "msmarco_gin - Index Size",
             "value": 824.93,
+            "unit": "MB"
+          }
+        ]
+      }
+    ],
+    "wikipedia_gin Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Todd J. Green",
+            "username": "tjgreen42",
+            "email": "tj@timescale.com"
+          },
+          "committer": {
+            "name": "Todd J. Green",
+            "username": "tjgreen42",
+            "email": "tj@timescale.com"
+          },
+          "id": "7d0b7056e70944c45d2e374121b02421cf5afe3d",
+          "message": "style: fix clang-format issues in build.c and state.h",
+          "timestamp": "2026-02-27T22:45:58Z",
+          "url": "https://github.com/timescale/pg_textsearch/commit/7d0b7056e70944c45d2e374121b02421cf5afe3d"
+        },
+        "date": 1772245813121,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wikipedia_gin - Index Build Time",
+            "value": 3785.511,
+            "unit": "ms"
+          },
+          {
+            "name": "wikipedia_gin - Index Size",
+            "value": 43.12,
             "unit": "MB"
           }
         ]
