5c8171dfba5a70e0f6301eb51b2c8d5a6e168697
Todd Green <greentodd@microsoft.com>
2026-05-11 15:08:45 +0000

    Release v1.2.0

--- src/mod.c
+++ src/mod.c
@@ -33,7 +33,7 @@
 #include "scoring/bm25.h"
 
 #if PG_VERSION_NUM >= 180000
-PG_MODULE_MAGIC_EXT(.name = "pg_textsearch", .version = "1.2.0-dev");
+PG_MODULE_MAGIC_EXT(.name = "pg_textsearch", .version = "1.2.0");
 #else
 PG_MODULE_MAGIC;
 #endif
