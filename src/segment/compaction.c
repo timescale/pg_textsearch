86d66b3d1ebd1c3e73ebc306aef21fe65d787770
Todd J. Green <tjgreen@gmail.com>
2026-09-17 20:22:35 +0000

    Include lock manager declarations explicitly

--- src/segment/compaction.c
+++ src/segment/compaction.c
@@ -14,6 +14,7 @@
 #include <storage/bufmgr.h>
 #include <storage/indexfsm.h>
 #include <storage/lmgr.h>
+#include <storage/lock.h>
 #include <storage/lwlock.h>
 #include <utils/hsearch.h>
 #include <utils/timestamp.h>
